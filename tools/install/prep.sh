750d1ea49c37d2662492257583e2b6d7f694a377
Steve Clement <steve@localhost.lu>
2012-02-06 10:46:01 +0100

    update prep.sh to 9.0 - perl options update

--- tools/install/prep.sh
+++ tools/install/prep.sh
@@ -3,27 +3,27 @@
 cd /usr/ports/ports-mgmt/portconf && make install clean
 pkg_add -r git python gmake automake14 libtool
 pkg_delete git-\*
-pkg_delete \*-freebsd-doc-\*
-pkg_add -r en-freebsd-doc fr-freebsd-doc de-freebsd-doc
+##pkg_delete \*-freebsd-doc-\*
+##pkg_add -r en-freebsd-doc fr-freebsd-doc de-freebsd-doc
 # if this fails: portsnap fetch extract
 
-mkdir /var/db/ports/python26
+##mkdir /var/db/ports/python26
 mkdir /var/db/ports/python27
 mkdir /var/db/ports/libiconv
 mkdir /var/db/ports/git
 
-cat << EOF > /var/db/ports/python26/options
-_OPTIONS_READ=python26-2.6.7_2
-WITH_THREADS=true
-WITHOUT_SEM=true
-WITHOUT_PTH=true
-WITH_UCS4=true
-WITH_PYMALLOC=true
-WITH_IPV6=true
-WITHOUT_FPECTL=true
-EOF
+##cat << EOF > /var/db/ports/python26/options
+##_OPTIONS_READ=python26-2.6.7_2
+##WITH_THREADS=true
+##WITHOUT_SEM=true
+##WITHOUT_PTH=true
+##WITH_UCS4=true
+##WITH_PYMALLOC=true
+##WITH_IPV6=true
+##WITHOUT_FPECTL=true
+##EOF
 
-cat << EOF > /var/db/ports/python26/options
+cat << EOF > /var/db/ports/python27/options
 _OPTIONS_READ=python27-2.7.2_3
 WITH_THREADS=true
 WITHOUT_SEM=true
