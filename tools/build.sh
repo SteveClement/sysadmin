9c0a0dd1c9354ffe6d8bf7ccfb90eae7cf4b1da8
Steve Clement <steve@localhost.lu>
2015-01-20 12:20:14 +0100

    - Added libevent2 (tmux dep) - update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -92,7 +92,7 @@ if [ -f ${TMP}/${VERSION} ]
   touch ${TMP}/${VERSION}
 fi
 
-for PORT in `echo misc_help2man ftp_wget sysutils_daemontools devel_gettext-tools mail_qmail ports-mgmt_portupgrade sysutils_ucspi-tcp shells_bash-completion net_rsync security_gnupg textproc_docbook-xsl lang_ruby20 databases_p5-DBI graphics_jbigkit graphics_jpeg devel_readline databases_db5 graphics_tiff devel_m4 graphics_png databases_gdbm math_gmp security_p5-IO-Socket-SSL databases_sqlite3 net_p5-Net-Server devel_p5-Module-Build print_freetype2 editors_vim devel_cscope devel_libffi databases_ruby-bdb databases_db41 sysutils_tmux devel_pcre archivers_unzip archivers_zip devel_p5-DateTime devel_p5-DateTime-Locale security_nmap dns_libidn www_serf devel_subversion lang_tcl86 dns_p5-Net-DNS www_p5-libwww devel_p5-DateTime-TimeZone security_libtasn1 textproc_py-pygments textproc_py-docutils security_nettle devel_libcheck devel_py-babel devel_icu devel_py-Jinja2 security_pinentry security_trousers security_gnutls devel_cmake www_lynx devel_apr1`; do
+for PORT in `echo misc_help2man ftp_wget sysutils_daemontools devel_gettext-tools mail_qmail ports-mgmt_portupgrade sysutils_ucspi-tcp shells_bash-completion net_rsync security_gnupg textproc_docbook-xsl lang_ruby20 databases_p5-DBI graphics_jbigkit graphics_jpeg devel_readline databases_db5 graphics_tiff devel_m4 graphics_png databases_gdbm math_gmp security_p5-IO-Socket-SSL databases_sqlite3 net_p5-Net-Server devel_p5-Module-Build print_freetype2 editors_vim devel_cscope devel_libffi databases_ruby-bdb databases_db41 sysutils_tmux devel_pcre archivers_unzip archivers_zip devel_p5-DateTime devel_p5-DateTime-Locale security_nmap dns_libidn www_serf devel_subversion lang_tcl86 dns_p5-Net-DNS www_p5-libwww devel_p5-DateTime-TimeZone security_libtasn1 textproc_py-pygments textproc_py-docutils security_nettle devel_libcheck devel_py-babel devel_icu devel_py-Jinja2 security_pinentry security_trousers security_gnutls devel_cmake www_lynx devel_apr1 devel_libevent2`; do
   mkdir -p ${PORTS_OPTIONS}/${PORT}
   cp ${REPO}${CONFIGS_COMMON}${PORTS_OPTIONS}/${PORT}/options ${PORTS_OPTIONS}/${PORT}
 done
@@ -174,10 +174,6 @@ if [ -f /usr/local/bin/bash ]
   cat /var/qmail/control/*
   sleep 15
 
-
-  ##echo "Copying Standard isoqlog conf file"
-  ##cp /usr/local/etc/isoqlog.conf-dist /usr/local/etc/isoqlog.conf
-  ##cp /usr/local/etc/isoqlog.domains-dist /usr/local/etc/isoqlog.domains
   svn co svn://svn.freebsd.org/base/releng/10.1 /usr/src
 
 if [ "$JAIL" = "false" ]; then
